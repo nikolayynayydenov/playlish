bd0ef552ca3474e081446486d97d749a0e9f8c44
nikolay <nikolayynayydenov@gmail.com>
2021-06-12 16:36:07 +0300

    link catch2 testing library

--- playlish/playlish.cpp
+++ playlish/playlish.cpp
@@ -1,9 +1,14 @@
+#define CATCH_CONFIG_RUNNER
 #include "./Common/DB.h"
 #include "./Common/Navigator.h"
+
+#include "./Tests/catch_amalgamated.hpp"
 
 int main(int argc, char* argv[]) {
 	try {
 		Navigator::goTo("menu");
+
+		int result = Catch::Session().run(argc, argv);
 	}
 	catch (SAException& x) {
 		DB::conn().Rollback();
